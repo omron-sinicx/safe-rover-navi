1cee1a2805ec27e818c05fedf19bba8e76de781b
masafumiendo <masafumi.endo@keio.jp>
2023-08-12 18:27:34 +0900

    polish and execute training script

--- scripts/train.py
+++ scripts/train.py
@@ -7,7 +7,6 @@ import random
 import sys, os
 BASE_PATH = os.path.dirname(__file__)
 sys.path.append(os.path.join(BASE_PATH, '../planning_project'))
-import dataclasses
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,8 +15,10 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 import segmentation_models_pytorch as smp
 
-from planning_project.utils.data import DataSet, visualize, create_int_label
 from planning_project.models.unet import Unet
+from planning_project.utils.data import DataSet, create_int_label
+from planning_project.utils.viz import viz_terrain_props
+from planning_project.utils.structs import TrainParams
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -38,38 +39,9 @@ def fix_randomness(seed):
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
 
-@dataclasses.dataclass
-class HyperParams:
-    """
-    structure containing hyper parameters
-
-    :param data_dir: directory to dataset
-    :param nn_model_dir: direcotry to neural network model
-    :param log_dir: directory to logger
-    :param lr: learning rate
-    :param th: metrics threshold
-    :param batch_size_train: batch size for training 
-    :param num_epochs: number of epochs
-    :param epoch_lr: number of epochs that decreases learning rate
-    :param patience: early stopping condition
-    :param max_score: max score
-    """
-    data_dir: str
-    nn_model_dir: str
-    log_dir: str
-
-    lr: float = 1e-3
-    th: float = 0.5
-    batch_size_train: int = 8
-    batch_size_valid: int = 1
-    num_epochs: int = 50
-    epoch_lr: int = 10
-    patience: int = 5
-    max_score: float = 0
-
 class Runner:
 
-    def __init__(self, model, hyper_params, seed: int = 0):
+    def __init__(self, model, train_params, seed: int = 0):
         """
         __init__:
 
@@ -79,24 +51,29 @@ class Runner:
         self.seed = seed
 
         self.model = model
-        self.hyper_params = hyper_params
+        self.train_params = train_params
 
+        # make directory if not
+        if not os.path.exists(self.train_params.nn_model_dir):
+            os.makedirs(self.train_params.nn_model_dir, exist_ok=True)
+        if not os.path.exists(self.train_params.log_dir):
+            os.makedirs(self.train_params.log_dir, exist_ok=True)
         self.loss = smp.utils.losses.CrossEntropyLoss()
-        self.metrics = [smp.utils.metrics.Accuracy(self.hyper_params.th)]
-        self.optimizer = torch.optim.Adam([dict(params=self.model.parameters(), lr=self.hyper_params.lr)])
+        self.metrics = [smp.utils.metrics.Accuracy(self.train_params.th)]
+        self.optimizer = torch.optim.Adam([dict(params=self.model.parameters(), lr=self.train_params.lr)])
         
     def run_trainval(self):
         """
         run_trainval: do training and validation for given network model
 
         """
-        train_loader = self.create_dataloader("train", batch_size=self.hyper_params.batch_size_train, shuffle=True)
-        valid_loader = self.create_dataloader("valid", batch_size=self.hyper_params.batch_size_valid, shuffle=False)
+        train_loader = self.create_dataloader("train", batch_size=self.train_params.batch_size_train, shuffle=True)
+        valid_loader = self.create_dataloader("valid", batch_size=self.train_params.batch_size_valid, shuffle=False)
         train_epoch, valid_epoch = self.create_epoch_runner()
         # main loop for network training
-        max_score = self.hyper_params.max_score
-        writer = SummaryWriter(log_dir=self.hyper_params.log_dir)
-        for i in range(self.hyper_params.num_epochs):
+        max_score = self.train_params.max_score
+        writer = SummaryWriter(log_dir=self.train_params.log_dir)
+        for i in range(self.train_params.num_epochs):
             print(f"Epoch:{i+1}")
             train_logs = train_epoch.run(train_loader)
             valid_logs = valid_epoch.run(valid_loader)
@@ -107,16 +84,16 @@ class Runner:
             writer.add_scalar("valid/accuracy", valid_logs["accuracy"], i)
             if max_score < valid_logs["accuracy"]:
                 max_score = valid_logs["accuracy"]
-                torch.save(self.model, self.hyper_params.nn_model_dir + "best_model.pth")
+                torch.save(self.model, self.train_params.nn_model_dir + "best_model.pth")
                 print("Model saved!")
                 early_stop_counter = 0
             else:
                 early_stop_counter += 1
                 print(f"not improve for {early_stop_counter}Epoch")
-                if early_stop_counter == self.hyper_params.patience:
+                if early_stop_counter == self.train_params.patience:
                     print(f"early stop. Max Score {max_score}")
                     break
-            if i == self.hyper_params.epoch_lr:
+            if i == self.train_params.epoch_lr:
                 self.optimizer.param_groups[0]["lr"] = 1e-5
                 print("Decrease decoder learning rate to 1e-5")
         writer.close()
@@ -127,7 +104,7 @@ class Runner:
 
         :param color_map: color map as network inputs
         """
-        best_model = torch.load(self.hyper_params.nn_model_dir + 'best_model.pth', map_location=torch.device(DEVICE))
+        best_model = torch.load(self.train_params.nn_model_dir + 'best_model.pth', map_location=torch.device(DEVICE))
         input = torch.tensor(color_map).unsqueeze(0)
         pred = best_model(input.to(DEVICE)) # torch.size([1, dim, n, n])
         pred = pred[0].cpu().detach().numpy() # np.array([dim, n, n])
@@ -144,7 +121,7 @@ class Runner:
         """
         g = torch.Generator()
         g.manual_seed(self.seed)
-        dataset = DataSet(self.hyper_params.data_dir, split)
+        dataset = DataSet(self.train_params.data_dir, split)
         dataloader = DataLoader(
             dataset, 
             batch_size=batch_size, 
@@ -183,32 +160,32 @@ class Runner:
         )
         return train_epoch, valid_epoch
 
-def predict_likelihood(color_map, hyper_params):
+def predict_likelihood(color_map, train_params):
     """
     predict: predict terrain classes using trained networks
 
     :param color_map: color map as network inputs
     """
-    best_model = torch.load(hyper_params.nn_model_dir + 'best_model.pth', map_location=torch.device(DEVICE))
+    best_model = torch.load(train_params.nn_model_dir + 'best_model.pth', map_location=torch.device(DEVICE))
     input = torch.tensor(color_map).unsqueeze(0)
     pred = best_model(input.to(DEVICE)) # torch.size([1, dim, n, n])
     pred = pred[0].cpu().detach().numpy() # np.array([dim, n, n])
     pred = np.max(pred, axis=0) # np.array([n, n])
     return pred
 
-def prediction(runner, hyper_params: str, iters: int = 5):
+def prediction(runner, train_params: str, iters: int = 5):
     """
     prediction: pick up several samples and predict corresponding terrain classes
 
     :param runner: object for prediction
     :param iters: # of iterations
     """
-    dataset = DataSet(hyper_params.data_dir, "test")
+    dataset = DataSet(train_params.data_dir, "test")
     for i in range(iters):
         color_map, mask = dataset[i]
         pred = runner.predict(color_map)
-        likelihood = predict_likelihood(color_map, hyper_params)
-        visualize(
+        likelihood = predict_likelihood(color_map, train_params)
+        viz_terrain_props(
             vmin=0,
             vmax=9,
             image=dataset.to_image(color_map), 
@@ -221,19 +198,23 @@ def main():
     seed = 0
     fix_randomness(seed=seed)
 
-    n_data = 1
-    data_dir = os.path.join(BASE_PATH, '../datasets/data%02d/' % (n_data))
-    nn_model_dir = os.path.join(BASE_PATH, '../trained_models/models/data%02d/' % (n_data))
-    log_dir = os.path.join(BASE_PATH, '../trained_models/logs')
-    is_trainval = False
+    n_ds = 'Std'
+    if n_ds != 'AA':
+        n_terrains = 10
+    else:
+        n_terrains = 8
+    data_dir = os.path.join(BASE_PATH, '../datasets/dataset_%s/' % (n_ds))
+    nn_model_dir = os.path.join(BASE_PATH, '../trained_models/models/dataset_%s/' % (n_ds))
+    log_dir = os.path.join(BASE_PATH, '../trained_models/logs/dataset_%s/' % (n_ds))
+    is_trainval = True
 
-    model = Unet(n_terrains=10, in_channels=3).set_model()
-    hyper_params = HyperParams(data_dir, nn_model_dir, log_dir)
-    runner = Runner(model, hyper_params, seed=seed)
+    model = Unet(n_terrains=n_terrains, in_channels=3).set_model()
+    train_params = TrainParams(data_dir, nn_model_dir, log_dir)
+    runner = Runner(model, train_params, seed=seed)
     if is_trainval:
         runner.run_trainval()
     else:
-        prediction(runner, hyper_params)
+        prediction(runner, train_params)
         
 if __name__ == '__main__':
     main()
